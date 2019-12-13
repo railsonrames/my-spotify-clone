ff9b54f90b989797712fe31c8de9ed1906f84ade
railsonrames <rames.dev@outlook.com>
2019-12-13 11:59:16 -0300

    Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-import {Swich, Route} from 'react-router-dom'
+import { Swich, Route } from 'react-router-dom'
 
-const Route () => (
+const Route = () => (
   <Swich>
-    <Route>
-      
-    </Route>
+    <Route></Route>
   </Swich>
 )
