0e9063853bf773350ea940af52b072e63b825877
railsonrames <rames.dev@outlook.com>
2020-03-01 00:59:08 -0300

    Adding reactotron, json-server fake api.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
+import './config/reactotron'
 import './styles/global'
 
 import Sidebar from './components/Sidebar/index'
@@ -10,19 +12,22 @@ import Header from './components/Header/index'
 import { Wrapper, Container, Content } from './styles/components'
 
 import Routes from './routes'
+import store from './store'
 
 const App = () => (
-  <BrowserRouter>
-    <Wrapper>
-      <Container>
-        <Sidebar />
-        <Content>
-          <Header />
-          <Routes />
-        </Content>
-      </Container>
-      <Player />
-    </Wrapper>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Wrapper>
+        <Container>
+          <Sidebar />
+          <Content>
+            <Header />
+            <Routes />
+          </Content>
+        </Container>
+        <Player />
+      </Wrapper>
+    </BrowserRouter>
+  </Provider>
 )
 export default App
